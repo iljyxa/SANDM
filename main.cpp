a1dbd949c139c62415a86b6356edb4b0b8533aaf
Ilya <ilja.fedorov@gmail.com>
2025-06-13 11:37:48 +0700

    update

--- main.cpp
+++ main.cpp
@@ -8,19 +8,7 @@ int main(int argc, char* argv[]) {
     app.setWindowIcon(QIcon(":/resources/icons/icon.svg"));
     app.setApplicationName("SANDM");
     app.setApplicationVersion(APP_VERSION_STRING);
-    app.setOrganizationName("Organization");
     QApplication::setFont(QFont ("Droid Sans Mono"));
-
-    /*
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale: uiLanguages) {
-        const QString baseName = "SANDM_IDE_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }*/
 
     MainWindow main_window;
     main_window.showMaximized();
